46724e96bc40c633e2d825f779499b1603a8de0b
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-09 11:36:16 -0700

    set up redux to check for current user

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+// import { bindActionCreators } from 'redux'
 
 import Landing from './Landing'
 import Home from './Home'
+// const API = process.env.REACT_APP_API
 
-const API = `${process.env.API}`
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      tickets: []
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     tickets: []
+  //   }
+  // }
 
-  async componentDidMount() {
-    const tickets = await fetch(`${API}/tickets.json`)
-    console.log(tickets);
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchUser()
+    // const responseGroups = await fetch(`${API}/groups`)
+    // const groups = await responseGroups.json()
+    // console.log(groups);
+    // this.dispatch({type: 'FETCH_USER'})
   }
 
   render() {
+    console.log('props from app render', this.props);
     return (
       <div className="div">
         <BrowserRouter>
@@ -34,4 +42,12 @@ class App extends Component {
   }
 }
 
-export default App
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+// const mapDispatchToProps = dispatch => ({
+//    actions: bindActionCreators(actions, dispatch)
+// })
+
+export default connect(mapStateToProps, actions)(App)
