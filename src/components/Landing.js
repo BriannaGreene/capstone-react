7fa69ffabb50648bbd7e814d13a2fadc46b9215a
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-12 17:38:16 -0700

    style updates

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,7 @@ const Landing = () => {
   return (
     <div>
       Landing Page
+      <a href='https://www.freepik.com/free-vector/funny-dinos-pattern_870965.htm'>Designed by Freepik</a>
     </div>
   )
 }
