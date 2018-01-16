e1dd02dd7cca6c0b2b4192dc7885d68738e8166c
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-15 19:54:40 -0700

    icons

--- src/components/home-components/FirstNav.js
+++ src/components/home-components/FirstNav.js
@@ -6,16 +6,16 @@ const FirstNav = () => {
     <div id="first-nav">
       <div id="icon-holder">
         <div id="avatar" className="sidebar-icon">
-          <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+          {/* <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i> */}
         </div>
         <div id="home-icon" className="sidebar-icon">
-          <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+          {/* <i className="fa fa-home fa-2x" aria-hidden="true"></i> */}
         </div>
         <div id="search-icon" className="sidebar-icon">
-          <i className="fa fa-search fa-2x" aria-hidden="true"></i>
+          {/* <i className="fa fa-search fa-2x" aria-hidden="true"></i> */}
         </div>
         <div id="settings-icon" className="sidebar-icon">
-          <i className="fa fa-cog fa-2x" aria-hidden="true"></i>
+          {/* <i className="fa fa-cog fa-2x" aria-hidden="true"></i> */}
         </div>
       </div>
     </div>
